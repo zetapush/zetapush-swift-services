ba28d1e4875d04ff2fece273a79048d3410f2dca
Morvan Mikael <mikaelmorvan@gmail.com>
2017-04-27 13:34:20 +0200

    remove Pods directory

--- Example/Pods/Target Support Files/ZetaPushSwift/ZetaPushSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZetaPushSwift : NSObject
-@end
-@implementation PodsDummy_ZetaPushSwift
-@end
--- Example/Pods/Target Support Files/ZetaPushSwiftServices/ZetaPushSwiftServices-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZetaPushSwiftServices : NSObject
-@end
-@implementation PodsDummy_ZetaPushSwiftServices
-@end
